f2515df8d226cc3a0e37be732cbb4d146f065d64
faf4r <edgecage@foxmail.com>
2024-06-27 17:45:49 +0800

    fix: see README. relogin

--- User.py
+++ User.py
@@ -12,19 +12,25 @@ from lxml import etree
 import utils
 from config import *
 
+
 # 递归遍历深度遍历列表
 def deep_iter(ndim_list):
     ret = []
+
     def inner_func(n_list):
         for i in n_list:
             if type(i) != list:
                 ret.append(i)
             else:
                 inner_func(i)
+
     return ret
+
 
 class User:
     def __init__(self, username, password, mail_config):
+        self.username = username
+        self.password = password
         self.ss = self.login(username, password)
         print("【登录成功!】")
         self.mail_config = mail_config
@@ -36,10 +42,10 @@ class User:
                 ss = utils.login(username, password)
                 return ss
             except ValueError as msg:
-                print(f'【{datetime.now()}】')
+                print(f"【{datetime.now()}】")
                 print(msg)
             except Exception as e:
-                print(f'*******************\n意外报错： {datetime.now()}\n', e)
+                print(f"*******************\n意外报错： {datetime.now()}\n", e)
                 sys.exit()
 
     def send(self, subject, text):
@@ -51,16 +57,16 @@ class User:
         按课程代码查询可选课程，返回可选课程的相关信息
         会去除掉不可选的
         """
-        query_url = 'http://jwc.swjtu.edu.cn/vatuu/CourseStudentAction'
+        query_url = "http://jwc.swjtu.edu.cn/vatuu/CourseStudentAction"
         data = {
-            'setAction': 'studentCourseSysSchedule',
-            'viewType': '',
-            'jumpPage': 1,
-            'selectAction': 'CourseCode',
-            'key1': code,
-            'courseType': 'all',
-            'key4': '',
-            'btn': '执行查询'
+            "setAction": "studentCourseSysSchedule",
+            "viewType": "",
+            "jumpPage": 1,
+            "selectAction": "CourseCode",
+            "key1": code,
+            "courseType": "all",
+            "key4": "",
+            "btn": "执行查询",
         }
         res = self.ss.post(url=query_url, data=data)
         # res.encoding = res.apparent_encoding
@@ -69,22 +75,24 @@ class User:
         ret = []
         for item in data:
             dic = {
-                'teacher': item.xpath('td[9]/a/span/text()')[0].strip(),
-                'course': item.xpath('td[4]/a/span/text()')[0].strip(),
-                'course_code': item.xpath('td[3]/a/span/text()')[0].strip(),
-                'date': item.xpath('td[11]/text()')[0].strip(),
-                'selected': item.xpath('td[13]/text()')[0].strip(),  # 如果满了就是''
-                'teachId': item.xpath('td[2]/span/text()')[0].strip()  # 选课参数
+                "teacher": item.xpath("td[9]/a/span/text()")[0].strip(),
+                "course": item.xpath("td[4]/a/span/text()")[0].strip(),
+                "course_code": item.xpath("td[3]/a/span/text()")[0].strip(),
+                "date": item.xpath("td[11]/text()")[0].strip(),
+                "selected": item.xpath("td[13]/text()")[0].strip(),  # 如果满了就是''
+                "teachId": item.xpath("td[2]/span/text()")[0].strip(),  # 选课参数
             }
             ret.append(dic)
         return ret
 
     def select_course(self, teachId):
         """选课操作"""
-        url = f'http://jwc.swjtu.edu.cn/vatuu/CourseStudentAction?setAction=addStudentCourseApply&teachId={teachId}&isBook=1&tt={int(time.time() * 1000)}'
+        url = f"http://jwc.swjtu.edu.cn/vatuu/CourseStudentAction?setAction=addStudentCourseApply&teachId={teachId}&isBook=1&tt={int(time.time() * 1000)}"
         res = self.ss.get(url=url)
         # res.encoding = res.apparent_encoding
-        msg = re.findall('<message>(.*?)</message>', res.text)[0]
+        if "未登陆" in res.text or "未登录" in res.text or "没有操作权限" in res.text:
+            raise ZeroDivisionError("登录过期")
+        msg = re.findall("<message>(.*?)</message>", res.text)[0]
         return msg
 
     def del_course(self, listId, chooseId):
@@ -93,7 +101,7 @@ class User:
         :param chooseId: 选课编号，如B2639
         :return:
         """
-        url = f'http://jwc.swjtu.edu.cn/vatuu/CourseStudentAction?setAction=delStudentCourseList&listId={listId}&teachId={chooseId}&tt={int(time.time() * 1000)}'
+        url = f"http://jwc.swjtu.edu.cn/vatuu/CourseStudentAction?setAction=delStudentCourseList&listId={listId}&teachId={chooseId}&tt={int(time.time() * 1000)}"
         res = self.ss.get(url=url)
         # res.encoding = res.apparent_encoding
         return res.text
@@ -102,16 +110,16 @@ class User:
 
     def select_course_by_chooseId(self, chooseId):
         # 先查询选课编号，找到teachId
-        url = 'http://jwc.swjtu.edu.cn/vatuu/CourseStudentAction'
+        url = "http://jwc.swjtu.edu.cn/vatuu/CourseStudentAction"
         data = {
-            'setAction': 'studentCourseSysSchedule',
-            'viewType': '',
-            'jumpPage': 1,
-            'selectAction': 'TeachID',
-            'key1': chooseId,
-            'courseType': 'all',
-            'key4': '',
-            'btn': '执行查询'
+            "setAction": "studentCourseSysSchedule",
+            "viewType": "",
+            "jumpPage": 1,
+            "selectAction": "TeachID",
+            "key1": chooseId,
+            "courseType": "all",
+            "key4": "",
+            "btn": "执行查询",
         }
         res = self.ss.post(url=url, data=data)
         # res.encoding = res.apparent_encoding
@@ -122,56 +130,71 @@ class User:
     # print(select_course_by_chooseId('B1532'))
 
     def del_course_by_chooseId(self, chooseId):
-        url = 'http://jwc.swjtu.edu.cn/vatuu/CourseStudentAction?setAction=studentCourseSysList&viewType=delCourse'
+        url = "http://jwc.swjtu.edu.cn/vatuu/CourseStudentAction?setAction=studentCourseSysList&viewType=delCourse"
         res = self.ss.get(url)
         html = etree.HTML(res.text)
         elements = html.xpath('//*[@id="table3"]/tr')[1:-1]
         for element in elements:
             # print(element.xpath('td[3]/text()')[0].strip())
-            if element.xpath('td[3]/text()')[0].strip() == chooseId:
+            if element.xpath("td[3]/text()")[0].strip() == chooseId:
                 break
-        listId = element.xpath('td[12]/input/@onclick')[0].strip().split(',')[-2].strip('\'"')
+        listId = (
+            element.xpath("td[12]/input/@onclick")[0]
+            .strip()
+            .split(",")[-2]
+            .strip("'\"")
+        )
         # print(listId)
         self.del_course(chooseId=chooseId, listId=listId)
 
     def del_courses_with_chooseIdList(self, chooseId_list):
-        url = 'http://jwc.swjtu.edu.cn/vatuu/CourseStudentAction?setAction=studentCourseSysList&viewType=delCourse'
+        url = "http://jwc.swjtu.edu.cn/vatuu/CourseStudentAction?setAction=studentCourseSysList&viewType=delCourse"
         res = self.ss.get(url)
         html = etree.HTML(res.text)
         elements = html.xpath('//*[@id="table3"]/tr')[1:-1]
         for element in elements:
             # print(element.xpath('td[3]/text()')[0].strip())
-            chooseId = element.xpath('td[3]/text()')[0].strip()
+            chooseId = element.xpath("td[3]/text()")[0].strip()
             if chooseId in chooseId_list:
-                listId = element.xpath('td[12]/input/@onclick')[0].strip().split(',')[-2].strip('\'"')
-                print(f'正在删除：{chooseId} {listId}')
+                listId = (
+                    element.xpath("td[12]/input/@onclick")[0]
+                    .strip()
+                    .split(",")[-2]
+                    .strip("'\"")
+                )
+                print(f"正在删除：{chooseId} {listId}")
                 # 有了上述提示信息，报错后可以选择使用del_course直接删除
                 self.del_course(chooseId=chooseId, listId=listId)
-                print(f'{chooseId}删除成功')
+                print(f"{chooseId}删除成功")
 
     def run_select_course(self, chooseId):
         while True:
             try:
                 msg = self.select_course_by_chooseId(chooseId)
-                print(f"course {chooseId}: ", end='')
+                print(f"course {chooseId}: ", end="")
                 print(msg)
-                if '选课成功' in msg:
+                if "选课成功" in msg:
                     break
-                elif '已选' in msg:
-                    break
-                elif '冲突' in msg:
-                    break
-                elif '选课申请成功' in msg:
-                    break
-                elif '成功' in msg:
-                    break
-                elif '优选班' in msg:
-                    break
+                # elif "已选" in msg:
+                #     break
+                # elif "冲突" in msg:
+                #     break
+                # elif "选课申请成功" in msg:
+                #     break
+                # elif "成功" in msg:
+                #     break
+                # elif "优选班" in msg:
+                #     break
+            except ZeroDivisionError:
+                try:
+                    self.ss = self.login(self.username, self.password)
+                except:
+                    print("登录过期，尝试登录中...")
             except IndexError:
-                print(f'【{datetime.now()}选课系统未开启】')
+                print(f"【{datetime.now()}选课系统未开启】")
             except Exception as e:
                 # self.send('全部成绩查询报错', str(e))
-                print(f'\n【error: {datetime.now()}】\n', e)
+                print(f"\n【error: {datetime.now()}】\n", e)
             time.sleep(1)
 
     def run_select_course_with_teachId(self, teachId, course):
@@ -184,37 +207,43 @@ class User:
         while True:
             try:
                 msg = self.select_course(teachId)
-                print(f"【{datetime.now()}】course {course}: ", end='')
+                print(f"【{datetime.now()}】course {course}: ", end="")
                 print(msg)
-                if '选课成功' in msg:
+                if "选课成功" in msg:
                     break
-                elif '已选' in msg:
-                    break
-                elif '冲突' in msg:
-                    break
-                elif '选课申请成功' in msg:
-                    break
-                elif '优选班' in msg:
-                    break
+                # elif '已选' in msg:
+                #     break
+                # elif '冲突' in msg:
+                #     break
+                # elif '选课申请成功' in msg:
+                #     break
+                # elif '优选班' in msg:
+                #     break
+            except ZeroDivisionError:
+                try:
+                    self.ss = self.login(self.username, self.password)
+                except:
+                    print("登录过期，尝试登录中...")
             except IndexError:
                 print(f"【{datetime.now()}】选课系统未开放...")
             except Exception as e:
                 # self.send('全部成绩查询报错', str(e))
-                print(f'\n{course} error: {datetime.now()}】\n', e)
+                print(f"\n{course} error: {datetime.now()}】\n", e)
             time.sleep(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # USAGE！！！先删除必要的课程，自行确定好要选的课程，收集teachId，然后选课
     user = User(username, password, config)
 
     # TODO：为了便捷和避免意外因素，可以考虑用cookie登录，临近选课时开冲
 
-
     # delete course by chooseId(课程编号，如B2492)
-    delete_courses = ['B2498']  # 要删除的课程的课程编号
+    delete_courses = ["B2498"]  # 要删除的课程的课程编号
     for i in delete_courses:
-        user.del_course_by_chooseId(i)  # 没有加异常处理，如有需要，请自行添加或选择手动删除课程
+        user.del_course_by_chooseId(
+            i
+        )  # 没有加异常处理，如有需要，请自行添加或选择手动删除课程
 
     #! 一下为使用teachId不经过查询直接选课
     # # 待选课程teachId，可以自己标注一下teachId代表的是什么课，便于多个选课返回结果时查看
@@ -227,7 +256,21 @@ if __name__ == '__main__':
     #     threads.append(t)
 
     # ! 以下为使用chooseId(选课编号)经过查询teachId选课
-    li = ['B2498', 'B2489', 'B2542', 'B3447', 'B1323', 'B2564', 'B2557', 'B2545', 'B2547', 'B2506', 'B2510', 'B2265', 'B1519']
+    li = [
+        "B2498",
+        "B2489",
+        "B2542",
+        "B3447",
+        "B1323",
+        "B2564",
+        "B2557",
+        "B2545",
+        "B2547",
+        "B2506",
+        "B2510",
+        "B2265",
+        "B1519",
+    ]
     threads = []
     for teachId in li:
         t = threading.Thread(target=user.run_select_course, args=(teachId,))
